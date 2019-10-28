9ee91826b69bf133c471e4df142b011c6b679f29
Spm <spm@renue.ru>
2019-10-28 15:25:26 +0500

    start project

---
+++ src/test/java/com/urfu/sod/SodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urfu.sod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
